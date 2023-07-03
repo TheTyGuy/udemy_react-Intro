0ba5ca29c52103bc55ee5bd7543ab19a57f0ddbe
TheTyGuy <102386303+TheTyGuy@users.noreply.github.com>
2023-07-03 18:12:31 -0500

    Create Car.jsx

---
+++ Car.jsx
@@ -0,0 +1,8 @@
+export function Car(props) {
+  const onClick = () => {
+    props.onCarClick(2);
+    //function onClick() {
+    //  props.onCarClick(2);
+  };
+  return <p onClick={onClick}> I am the {"<Car/>"}</p>;
+}
