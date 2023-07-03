289cfb44878fc9f3ae22610eb7e1db44c223c110
TheTyGuy <102386303+TheTyGuy@users.noreply.github.com>
2023-07-03 17:12:42 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import { Car } from "./Components/Car/Car";
+import { Greetings } from "./Components/Greetings/Greetings";
+import "./global.css";
+
+export function App() {
+  return (
+    <div>
+      <Car />
+      <Greetings />
+    </div>
+  );
+}
